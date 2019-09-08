20be6618b8d5261ae3817ad6ab79ee6f3015dfe5
emily <mle4d@me.com>
2019-09-07 19:32:27 -0700

    init

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './header.css';
+
+const Header = () => {
+
+  return (
+    <>
+      <h1>Emily Raquel Ford</h1>
+      <h3>Full stack Software Developer</h3>
+    </>
+  );
+};
+
+export default Header;
