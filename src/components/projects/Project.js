a7eec51fba48c71f10f64a17331fb46e974c3e73
emily <mle4d@me.com>
2019-09-16 09:06:34 -0700

    tyring to do flip cards

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -8,16 +8,27 @@ const Project = ({ name, image, description, link, github }) => {
 
   return (
 
-    <div className={styles.Project}>
-      <h3 className={styles.name}>{name}
-        <img className={styles.ProjectImage}src={image} alt=""/>
-        <p className={styles.description}><a className={styles.projectLinks} href={link}>Site</a>&nbsp;&nbsp;&nbsp;&nbsp;
-          <a className={styles.projectLinks} href={github}>Repo</a></p>
-        <section>{description}</section>
+  //  <div className={styles.Project}>
+  //    <h3 className={styles.name}>{name}
+  //      <img className={styles.ProjectImage}src={image} alt=""/>
+  //      <p className={styles.description}><a className={styles.projectLinks} href={link}>Site</a>&nbsp;&nbsp;&nbsp;&nbsp;
+  //        <a className={styles.projectLinks} href={github}>Repo</a></p>
+  //      <section>{description}</section>
       
-      </h3>
+  //      </h3>
+  //   </div>
+    <div className={styles.project}>
+      <div classNames={styles.projectInner}>
+        <div classNames={styles.projectFront}>
+          <img classNames={styles.image} src={image} />
+        </div>
+        <div classNames={styles.projectBack}>
+          <h1>{name}</h1>
+          <p>{github} & {link}</p>
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
-
   );
 
 };
