a46f885f87b120a12494dc21a54952840f52748e
emily <mle4d@me.com>
2019-09-29 16:39:11 -0700

    brought in images of works in progress

--- src/components/projects/SoloProjects.js
+++ src/components/projects/SoloProjects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Project from './Project';
 import meowmachine from '../../assets/meowmachine.png';
+import sanrio from '../../assets/sanrio.png';
+import emoji from '../../assets/emoji.png';
 import styles from './projects.css';
 
 
@@ -10,6 +12,8 @@ export default function SoloProjects() {
     <section className={styles.Projects}>
       <div className={styles.container}>
         <Project name="Meow Machine" image={meowmachine} stack="React, HTML, CSS"link="https://meowmachine.netlify.com/" github="https://github.com/mle4d/MeowMachine" />
+        <Project name="Sanrio API" image={sanrio} stack="Sanrio Character API. In Progress!"link="hold tight" github="it's coming" />
+        <Project name="Emoji Fullstack" image={emoji} stack="Fullstack Emoji Story Tweeter. Coming Soon!"link="hold tight" github="it's coming" />
       </div>
     </section>
   );
